50283c22100a0324eb1c15d72d9b0d818c325e2e
carson <carsonisalsome@gmail.com>
2024-08-11 18:45:43 +0800

    added code

--- source.cpp
+++ source.cpp
@@ -1,43 +1,75 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib> // Include for using system()
+
 using namespace std;
 
 int proc() {
-    int tsize;
-    cout << "How many cells: ";
-    cin >> tsize;
+    string usrans;
+    cout << "Are you on Windows or Linux: ";
+    cin >> usrans;
 
-    int tape[tsize] = {0}; // Initialize all cells to 0
-    int i = 0;
-    int* cptr = &tape[i]; // Pointer to the current cell
+    int tc = 1;
+    char t = '	'; // Use '\t' to represent a tab character
     string filename;
     cout << "Enter the file name: ";
     cin >> filename;
-
     ifstream file(filename);
+
+    int tsize;
+    cout << "How many cells: ";
+    cin >> tsize;
+    fstream bfs("bfsource.cpp", ios::out); // Open file in write mode
+
+    bfs << "#include<iostream>\n";
+    bfs << "using namespace std;\n";
+    bfs << "\n";
+    bfs << "int main() {\n";
+    bfs << "    int tape[" << tsize << "] = {0};\n";
+    bfs << "    int i = 0;\n";
+    bfs << "    int* cptr = &tape[i];\n";
+
     char c;
-    
+
     while (file.get(c)) {
+		string repeatedChar(tc, t);
         if (c == '>') {
-            i++;
-            cptr = &tape[i]; // Move the pointer to the next cell
+            bfs << repeatedChar << "i++;\n";
+            bfs << repeatedChar << "cptr = &tape[i];\n";
         } else if (c == '<') {
-            i--;
-            cptr = &tape[i]; // Move the pointer to the previous cell
+            bfs << repeatedChar << "i--;\n";
+            bfs << repeatedChar << "cptr = &tape[i];\n"; // Move the pointer to the previous cell
         } else if (c == '+') {
-            (*cptr)++; // Increment the value at the current cell
+            bfs << repeatedChar << "*cptr++;\n"; // Increment the value at the current cell
         } else if (c == '-') {
-            (*cptr)--; // Decrement the value at the current cell
+            bfs << repeatedChar << "*cptr--;\n"; // Decrement the value at the current cell
         } else if (c == '.') {
-            cout << *cptr; // Output the value at the current cell as a character
+            bfs << repeatedChar << "char sigma = static_cast<char>(*cptr);\n";
+            bfs << repeatedChar << "cout << sigma;\n"; // Output the value at the current cell as a character
         } else if (c == ',') {
-            cin >> *cptr; // Input a value to the current cell
-        } else {
-            continue; // Ignore other characters
+            bfs << repeatedChar << "cin >> *cptr;\n"; // Input a value to the current cell
+        } else if (c == '[') {
+            bfs << repeatedChar << "while (tape[0] != 0) {\n";
+            tc++;
+        } else if (c == ']') {
+            tc = tc - 1;
+			string repeatedChar(tc, t);
+            bfs << repeatedChar << "}\n";
         }
     }
+	bfs << "\n	return 0;\n";
+	bfs << "}";
 
+    if (usrans == "windows") {
+        system("g++ bfsource.cpp -o brainfuckoutput.exe");
+		system("brainfuckoutput.exe");
+	} else {
+        system("g++ bfsource.cpp -o brainfuckoutput");
+		system("./brainfuckoutput");
+    }
+
+    bfs.close(); // Close the file after writing
     return 0;
 }
 
